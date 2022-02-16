e1b9cbfa3d039a784641bb1d40727a90df2b2e4a
jinseonghee <wlstjdgml0610@gmail.com>
2022-02-16 22:54:12 +0900

    feat: 회원 등록 폼 개발

--- hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
@@ -2,7 +2,10 @@ package com.example.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.hellospring.domain.Member;
 import com.example.hellospring.service.MemberService;
 
 @Controller
@@ -13,5 +16,23 @@ public class MemberController {
 	@Autowired //생성자에 @Autowired 가 있으면 스프링이 연관된 객체를 스프링 컨테이너에서 찾아서 넣어준다. 이렇게 객체 의존관계를 외부에서 넣어주는 것을 DI (Dependency Injection), 의존성 주입이라 한다.
 	public MemberController(MemberService memberservice) {
 		this.memberservice = memberservice;
+	}
+
+	@GetMapping("/members/new")
+	public String create() {
+		return "members/createMemberForm";
+	}
+
+	@PostMapping("/members/new")
+	public String create(MemberForm form) {
+
+		Member member = new Member();
+		member.setName(form.getName());
+
+		memberservice.join(member);
+
+		//System.out.println("member = " + form.getName());
+
+		return "redirect:/";
 	}
 }
