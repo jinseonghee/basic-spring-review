844770d9ace4b129c2dcc568f4935e547987ee49
jinseonghee <wlstjdgml0610@gmail.com>
2022-02-16 22:11:02 +0900

    feat: 스프링 빈 등록

--- hello-spring/src/main/java/com/example/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/com/example/hellospring/HelloSpringApplication.java
@@ -1,4 +1,4 @@
-package com.example.hellospring;
+package com.example.hellospring; //이 패키지 하위 까지만 spring이 componentScan을 함. 그래서 그 외에 아무곳에나 @Bean등록 하려면 설정 바꿔줘야 함
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
