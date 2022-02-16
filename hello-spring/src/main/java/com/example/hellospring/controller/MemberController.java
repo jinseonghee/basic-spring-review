e57205ff7e2aa707f816320dc3d4feedb3d4ea78
jinseonghee <wlstjdgml0610@gmail.com>
2022-02-16 23:29:17 +0900

    feat: 회원 조회 기능 추가

--- hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.example.hellospring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -34,5 +37,12 @@ public class MemberController {
 		//System.out.println("member = " + form.getName());
 
 		return "redirect:/";
+	}
+
+	@GetMapping("/members")
+	public String list(Model model) {
+		List<Member> members = memberservice.findMembers(); //findMembers는 모든 member를 불러
+		model.addAttribute("members", members);
+		return "members/memberList";
 	}
 }
