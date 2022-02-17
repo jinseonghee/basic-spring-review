75b8775e1dfc5f606c7cdd14a012ee0cac4bf305
jinseonghee <wlstjdgml0610@gmail.com>
2022-02-17 22:56:19 +0900

    feat: Jdbc 회원 리포지토리

--- hello-spring/src/main/java/com/example/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,8 +1,11 @@
 package com.example.hellospring;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.hellospring.repository.JdbcMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
 import com.example.hellospring.repository.MemoryMemberRepository;
 import com.example.hellospring.service.MemberService;
@@ -10,12 +13,20 @@ import com.example.hellospring.service.MemberService;
 @Configuration
 public class SpringConfig {
 
+	private final DataSource dataSource;
+
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	@Bean
 	public MemberService memberservice(){
 		return new MemberService(memberRepository());
 	}
 
-	private MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+	@Bean
+	public MemberRepository memberRepository() {
+		//return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource); //개방-폐쇄 원칙(OCP, Open-Closed Principle) 확장에는 열려있고, 수정, 변경에는 닫혀있다.
 	}
 }
