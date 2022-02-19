7a207991c3ca5bb3b9f92f2c93324fd9d5064528
jinseonghee <wlstjdgml0610@gmail.com>
2022-02-19 13:18:48 +0900

    feat: 스프링 JdbcTemplate 회원 repository

--- hello-spring/src/main/java/com/example/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/example/hellospring/SpringConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.hellospring.repository.JdbcMemberRepository;
+import com.example.hellospring.repository.JdbcTemplateMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
 import com.example.hellospring.repository.MemoryMemberRepository;
 import com.example.hellospring.service.MemberService;
@@ -27,6 +28,7 @@ public class SpringConfig {
 	@Bean
 	public MemberRepository memberRepository() {
 		//return new MemoryMemberRepository();
-		return new JdbcMemberRepository(dataSource); //개방-폐쇄 원칙(OCP, Open-Closed Principle) 확장에는 열려있고, 수정, 변경에는 닫혀있다.
+		//return new JdbcMemberRepository(dataSource); //개방-폐쇄 원칙(OCP, Open-Closed Principle) 확장에는 열려있고, 수정, 변경에는 닫혀있다.
+		return new JdbcTemplateMemberRepository(dataSource);
 	}
 }
